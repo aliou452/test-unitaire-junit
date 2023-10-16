89ada4176da5e2f90abf66a85835d7ae7f06857c
Aliou SY <asy@we-it.co>
2023-10-16 14:34:22 +0200

    BeforeEach

--- src/test/java/org/exemple/calculs/CalculatorTest.java
+++ src/test/java/org/exemple/calculs/CalculatorTest.java
@@ -3,19 +3,28 @@ package org.exemple.calculs;
 
 import org.example.calculs.Calculator;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-
+@DisplayName("Class for testing Calculator")
 public class CalculatorTest {
 
+    Calculator defaultCalculator;
 
+    @BeforeEach
+    void getDefaultCalculator() {
+        defaultCalculator = new Calculator(2, 5);
+    }
 
 
     @Test
+    @DisplayName("Test calculator add method")
     void addTest() {
-        Calculator calculator = new Calculator(2, 5);
+        Calculator calculator = defaultCalculator;
 
         int res = calculator.add();
         int expectedValue = 7;
@@ -25,17 +34,19 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("Test calculator string method")
     void toStringTest() {
-        Calculator calculator = new Calculator(2, 8);
+        Calculator calculator = defaultCalculator;
 
         String string = calculator.toString();
-        String expectedValue = "num1:2; num2:8";
+        String expectedValue = "num1:2; num2:5";
 
         assertEquals(expectedValue, string);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
+    @DisplayName("Test pair method")
     void isPairTest(int num) {
 
         boolean result = Calculator.isPair(num);
@@ -44,6 +55,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("Test divise method")
     void diviseTest() {
         Calculator calculator = new Calculator(20, 4);
 
